bd412ec9977631f25e5ce765ba9a9d26337cd69e
dlsn3212 <dywjd2341@naver.com>
2020-07-01 11:05:00 +0900

    Mini

---
+++ dh11_base_test.py
@@ -0,0 +1,17 @@
+import Adafruit_DHT as dht
+import time
+sensor = dht.DHT11
+pin = 21 # 4 핀번호
+try:
+    while True:
+        h, t = dht.read_retry(sensor, pin)
+        if h is not None and t is not None:
+            print("Temp = {0:0.1f}C Humidity = {1:0.1f}%".format(t,h))
+        else:
+            print("Read error") 
+
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Terminated by Keyboard")
+finally:
+    print("End of program")
